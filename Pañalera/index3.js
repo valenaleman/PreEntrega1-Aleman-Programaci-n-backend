c1c07841b3b4d8c15fa89092aab65175c392d6e0
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2023-10-17 21:36:36 -0300

    agrego modificaciones de index3.js

--- Pañalera/index3.js
+++ Pañalera/index3.js
@@ -12,3 +12,19 @@ class Prodcto {
         console.log("Hola usted selecciono la clase productos");
     }
 }
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const combinarArray = [...new Set([...arr1, ...arr2])];
+console.log(combinarArray);
+
+function sumar(...numeros){
+    let total =0;
+    for(const numero of numeros){
+        total += numero;
+    }
+    return total;
+}
+
+console.log(sumar(1,2,3,4,5,6));
